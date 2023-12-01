86908bc4f2327f4012f5048c983cc3de47069840
Fabio dalfovo <fabio.dalfovo@zupit.it>
2023-12-01 19:17:36 +0100

    simple angular 16 application

--- frontend/angular16/TicTacToe/src/app/app.module.ts
+++ frontend/angular16/TicTacToe/src/app/app.module.ts
@@ -3,16 +3,42 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BoardComponent } from './board/board.component';
+import { CellComponent } from './cell/cell.component';
+import { GameStatusDisplayComponent } from './game-status-display/game-status-display.component';
+import { TicTacToeCellSate } from 'src/libs/tic-tac-toe/tic-tac-toe-cell-state';
+import { TicTacToeGameService } from 'src/libs/tic-tac-toe/tic-tac-toe-game.service';
+import { TicTacToeGameUtility } from 'src/libs/tic-tac-toe/tic-tac-toe-game.utility';
+
+/** TicTacToeGameService */
+const ticTacToeGameServiceFactory = (gameUtility: TicTacToeGameUtility) => {
+  return new TicTacToeGameService(
+    [
+      [TicTacToeCellSate.Empty, TicTacToeCellSate.Empty, TicTacToeCellSate.Empty],
+      [TicTacToeCellSate.Empty, TicTacToeCellSate.Empty, TicTacToeCellSate.Empty],
+      [TicTacToeCellSate.Empty, TicTacToeCellSate.Empty, TicTacToeCellSate.Empty]
+    ],
+    gameUtility
+  );
+};
+const ticTacToeGameServiceProvider = {
+  provide: TicTacToeGameService,
+  useFactory: ticTacToeGameServiceFactory,
+  deps: [TicTacToeGameUtility],
+};
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BoardComponent,
+    CellComponent,
+    GameStatusDisplayComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [
+    TicTacToeGameUtility,
+    ticTacToeGameServiceProvider
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
