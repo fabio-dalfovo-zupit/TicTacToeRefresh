303df652421d5b2514425de287ee22b4076f46f6
Fabio dalfovo <fabio.dalfovo@zupit.it>
2023-12-06 15:57:19 +0100

    start

--- frontend/apps/TicTacToeAngular16NgRx/src/app/game-status-display/game-status-display.component.ts
+++ frontend/apps/TicTacToeAngular16NgRx/src/app/game-status-display/game-status-display.component.ts
@@ -1,8 +1,33 @@
 import { Component } from '@angular/core';
+import { PlayerToPlay } from '@libs/tic-tac-toe-game';
 
 @Component({
   selector: 'tic-tac-toe-game-status-display',
   templateUrl: './game-status-display.component.html',
   styleUrls: ['./game-status-display.component.scss']
 })
-export class GameStatusDisplayComponent {}
+export class GameStatusDisplayComponent {
+//  /** Service for the game */
+//  public ticTacToeGameService: TicTacToeRxJsService;
+//
+//  constructor(ticToeGameService: TicTacToeRxJsService) {
+//    this.ticTacToeGameService = ticToeGameService;
+//  }
+
+  /** Return the name of the player */
+  public playerToName(player: PlayerToPlay | null): string {
+    switch (player) {
+      case PlayerToPlay.PlayerX:
+        return 'X';
+      case PlayerToPlay.PlayerO:
+        return 'O';
+      default:
+        return '';
+    }
+  }
+
+  /** Handle restart request */
+  public onRestartHandler(): void {
+//    this.ticTacToeGameService.resetBoard();
+  }
+}
